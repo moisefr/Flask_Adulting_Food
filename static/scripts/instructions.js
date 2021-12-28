c0d6676e7280776f6302c138a124b695591a0789
moisefr <38119115+moisefr@users.noreply.github.com>
2021-12-27 21:22:33 -0500

    udated movement flow
    can now use jquery hover to generate a list

--- static/scripts/instructions.js
+++ static/scripts/instructions.js
@@ -4,16 +4,27 @@
 // });
 
 $(function(){
+    var counter = 0;
     $("#inst_button").click(function(){
         // e.preventDefault()
+        
         value = $("#input-holder").val()
-        console.log(value)
         // new_input = "<div class='input-area'>" + label + input + button +"</div>"
         new_input = "<li>" + value +"</li>"
         $("#instruction_raw").append(new_input);
         $("li").draggable()
+        // console.log(counter)
+       
         // window.alert("huh")
       });
+    $(".holder").hover(function(){
+        counter = counter + 1;
+        // window.alert(counter)
+        counting = "<p>" + counter +"</p>"
+        $(".holder2").append(counting);
+        // window.alert('ya')
+    });
     // jQuery methods go here...
+    // $("#inner-grid").droppable()
 });
 
