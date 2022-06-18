e3152d0771488677915dd52eb819ffcdc3d30596
moisefr <38119115+moisefr@users.noreply.github.com>
2022-06-18 16:55:31 -0400

    try

--- server.py
+++ server.py
@@ -1367,6 +1367,6 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # if __name__ == "__main__":
 #App Run
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port, debug=True)
+# port = int(os.environ.get("PORT", 5000))
+app.run(debug=True)
     #ssl_context=('cert.pem', 'key.pem')
