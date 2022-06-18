be8403051e22c5327ed2451e0b9e1af08e3e0003
moisefr <38119115+moisefr@users.noreply.github.com>
2022-06-18 15:10:34 -0400

    Update server.py

--- server.py
+++ server.py
@@ -130,7 +130,7 @@ def callback():
         return "Unsupported token type. Should be 'Bearer'.", 403
     access_token = exchange["access_token"]  
     id_token = exchange["id_token"]
-    print("#access TOKEN - 🟢: \n"f"{access_token}\n" +  " #identity TOKEN - 🔵: \n"f"{id_token}")
+    # print("#access TOKEN - 🟢: \n"f"{access_token}\n" +  " #identity TOKEN - 🔵: \n"f"{id_token}")
     if not is_access_token_valid(access_token, config["web"]["issuer"]):
         return "Access token is invalid", 403
     if not is_id_token_valid(id_token, config["web"]["issuer"], config["web"]["client_id"], NONCE):
